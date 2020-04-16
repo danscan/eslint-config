b45080297d73fe5c0a22aadb69c524640cc30f75
Dan Scanlon <dscanlonpa@gmail.com>
2020-04-16 00:54:36 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['react-hooks'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'max-len': 0,
+    'no-nested-ternary': 0,
+    'no-use-before-define': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-curly-newline': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 'warn',
+    'react/no-unescaped-entities': 0,
+    'react/prop-types': [1, { skipUndeclared: true }],
+    'spaced-comment': 0,
+
+    // Prettier
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
